b3478d9e8996f50481ae31c6e450848b22de0b45
Vitor Barbosa <Vitorbarbosa062@gmail.com>
2024-08-21 17:03:54 -0300

    Primeiro commit

---
+++ Models/Enuns/TipoFuncionarioEnum.cs
@@ -0,0 +1,8 @@
+namespace Aula03Colecoes.Models.Enuns
+{
+    public enum TipoFuncionarioEnum //Nome da Variável diferente do programa para evitar problemas.
+    {
+        Clt = 1,
+        Aprendiz = 2
+    }
+}
